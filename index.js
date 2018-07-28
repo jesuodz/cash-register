dbd338ae1501a9a76a59b4def2ce4a803abe953c
Jesus Ordosgoitty <jesuodz@gmail.com>
2018-07-28 13:10:45 -0400

    Return propper message for closed transaction

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const register = {};
 
-register.checkCash = function (price, cash, cid) {
-    let isEnough = (price >= cash) ? true : false;
-    let obj = {};
+register.checkCash = (price, cash, cid) => {
+    var output = { change: [] };
+    var changeDue = cash - price;
+
+    // What's in my register
+    var total = cid.reduce( (accum, currVal) => {
+        accum += currVal[1];
+        return accum;
+    }, 0);
+
+    // Register is empty
+    if (total === changeDue) {
+      output.status = 'CLOSED';
+      output.changeDue = cid;
+      return output;
+    }
 
     if (!isEnough) {
         obj.status = 'INSUFFICIENT_FUNDS';
         obj.change = [];
     }
 
-    return obj;
+    return output;
 }
 
 module.exports = register;
