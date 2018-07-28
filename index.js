6278ebc8fa9fc29ee6625f9271ac07391c637362
Jesus Ordosgoitty <jesuodz@gmail.com>
2018-07-28 15:18:28 -0400

    Add solution (possibly uncompleted)

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const register = {};
 
+var units = [
+    {name: 'ONE HUNDRED', value: 100.00},
+    {name: 'TWENTY', value: 20.00},
+    {name: 'TEN', value: 10.00},
+    {name: 'FIVE', value: 5.00},
+    {name: 'ONE', value: 1.00},
+    {name: 'QUARTER', value: 0.25},
+    {name: 'DIME', value: 0.10},
+    {name: 'NICKEL', value: 0.05},
+    {name: 'PENNY', value: 0.01}
+];
+  
 register.checkCash = (price, cash, cid) => {
     let output = { change: [] };
     let changeDue = cash - price;
@@ -24,6 +36,53 @@ register.checkCash = (price, cash, cid) => {
         return output;
     }
 
+    let registerUnits = cid.reduce( (accum, curr) => {
+        accum[curr[0]] = curr[1];
+        return accum;
+    }, {});
+
+    // Unfinished
+    // let changeArr = units.map((obj) => {
+    //     var value = 0;
+
+    //     for (var index = 0; index < cid.length; index++) {
+    //         if (cid[index][0] == obj.name && changeDue >= obj.value) {
+    //             changeDue =- obj.value;
+    //             cid[index] -= obj.value;
+    //             value += obj.value;
+
+    //             changeDue = Math.round(changeDue * 100) / 100;
+    //         }
+    //     }
+
+    //     if (value > 0) {
+    //         return [obj.name, value];
+    //     }
+    // });
+
+    let changeArr = units.reduce((acc,curr) => {
+        var value = 0;
+
+        while (registerUnits[curr.name] > 0 && changeDue >= curr.value) {
+            changeDue -= curr.value;
+            registerUnits[curr.name] -= curr.value;
+            value += curr.value;
+
+            changeDue = Math.round(changeDue * 100) / 100;
+        }
+        if (value > 0) {
+            acc.push([curr.name, value]);
+        }
+        return acc;
+    }, []);
+
+    if (changeArr.length < 1 || changeDue > 0) {
+        output.status = 'INSUFFICIENT_FUNDS';
+        return output;
+    }
+    // Here is your change, ma'am.
+    output.status = 'OPEN';
+    output.change = changeArr;
     return output;
 }
 
--- tests/start.js
+++ tests/start.js
@@ -3,5 +3,5 @@ const register = require('../index.js');
 console.log(
     register.checkCash(process.argv[2],
                         process.argv[3],
-                        process.argv[4])
+                        Array.prototype.slice.call(process.argv[4]))
 );
